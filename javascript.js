afa382dd408328a8a21dcafb64b95c0769669d7d
SunnyRoadsAdventures <xena.morphia.nilssin@gmail.com>
2026-02-12 06:10:23 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -25,3 +25,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+const mission = document.getElementById("missionVideo");
+const blackie = document.getElementById("blackieVideo");
+const hey = document.getElementById("heyVideo");
+
+// Hide new videos at start
+if (blackie) blackie.style.display = "none";
+if (hey) hey.style.display = "none";
+if (mission) mission.style.display = "none";
+
+// When mission ends → show blackie + hey
+if (mission) {
+    mission.addEventListener("ended", function () {
+
+        mission.style.display = "none";
+
+        blackie.style.display = "block";
+        hey.style.display = "block";
+
+        blackie.play();
+        hey.play();
+    });
+}
