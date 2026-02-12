6567605797642e7dfe1f6bacb3c064e5365ee34f
SunnyRoadsAdventures <xena.morphia.nilssin@gmail.com>
2026-02-12 12:46:10 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -142,3 +142,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+#marketImg {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    display: none;
+    z-index: 30;
+}
