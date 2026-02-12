f9cb368d352bb2ad47c7c29340d5205bec9c8e06
SunnyRoadsAdventures <xena.morphia.nilssin@gmail.com>
2026-02-12 04:56:58 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
 window.addEventListener("load", function () {
-    const video = document.getElementById("sraVideo");
 
-    // Small delay before fade
+    const video = document.getElementById("sraVideo");
+    const goBtn = document.getElementById("goBtn");
+
+    // Fade in video
     setTimeout(() => {
         video.style.opacity = "1";
     }, 500);
+
+    // Fade in GO button after video fade completes
+    setTimeout(() => {
+        goBtn.style.opacity = "1";
+    }, 2500);
+
 });
