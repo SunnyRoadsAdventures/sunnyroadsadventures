cecdea204e16396aef736fed26daee6ac9ef8569
SunnyRoadsAdventures <xena.morphia.nilssin@gmail.com>
2026-02-19 06:54:03 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+
+    setTimeout(function() {
+        document.querySelector(".white-fade").classList.add("active");
+        document.querySelector(".hero-overlay").classList.add("active");
+    }, 15000);
+
+});
