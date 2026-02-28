b2c0b8c69e852d467adec23b63634393959ae9ec
SunnyRoadsAdventures <xena.morphia.nilssin@gmail.com>
2026-02-28 10:39:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -12,17 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
     heroText.style.opacity = "0";  
 
     heroVideo.addEventListener("ended", () => {  
-      console.log("tower.mp4 ended"); // debug
-
-      // white fade
+      // White fade
       if (whiteFade) {  
         whiteFade.style.opacity = "1";  
       }  
 
-      // fade in text after white fade
+      // Fade in hero text
       setTimeout(() => {  
         heroText.classList.add("is-visible"); // CSS handles fade
-        console.log("hero text visible"); // debug
       }, 900);
     });
   }
