e3d54fbd69bdba18aea37a7db9766cad6ef9390a
SunnyRoadsAdventures <xena.morphia.nilssin@gmail.com>
2026-03-06 16:55:19 -0600

    Update java.js

--- english-learning/java.js
+++ english-learning/java.js
@@ -1,14 +1,20 @@
 const lessons = document.querySelectorAll(".lesson");
+
 const player = document.getElementById("videoPlayer");
+
 const video = document.getElementById("lessonVideo");
+
 
 lessons.forEach(lesson => {
 
 lesson.addEventListener("click", () => {
 
 if (lesson.classList.contains("locked")) {
+
 alert("Subscribe to unlock lessons.");
+
 return;
+
 }
 
 let src = lesson.getAttribute("data-video");
@@ -23,12 +29,38 @@ video.play();
 
 });
 
-function closeVideo() {
+
+function closeVideo(){
 
 video.pause();
 
+video.src = "";
+
 player.classList.add("hidden");
 
-video.src = "";
+}
+
+
+function unlockLessons(){
+
+let code = document.getElementById("unlockCode").value;
+
+if(code === "CURATED2026"){
+
+document.querySelectorAll(".lesson").forEach(l => {
+
+l.classList.remove("locked");
+
+});
+
+alert("Lessons unlocked. Welcome.");
+
+}
+
+else{
+
+alert("Invalid access code.");
+
+}
 
 }
